33e70020e6fa071079a10742880500da20d8b027
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-06-02 15:29:18 +0900

    Feat: add MANIFEST.MF

---
+++ Common/src/main/resources/init-schema.sql
@@ -0,0 +1,32 @@
+INSERT INTO recent (recent_id, count, created_at, image_url, price, rating, title)
+VALUES
+    (1, 500, '2024-06-01 15:56:47.992138', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/massagegun.jpeg', 200, 3.5, '마사지 건'),
+    (2, 600, '2024-06-01 16:00:04.354306', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/almondcream.jpeg', 10, 3.8, 'Almond'),
+    (3, 920, '2024-06-01 16:01:50.934168', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/aloecream.jpg', 11, 4.5, 'Aloe Vera'),
+    (4, 100, '2024-06-01 16:21:46.260021', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/ayurmassagecream.jpeg', 15, 4.2, 'Ayur'),
+    (5, 1000, '2024-06-01 16:23:51.436476', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/massagechair.jpeg', 697856, 4.6, 'Rilassa'),
+    (6, 110, '2024-06-01 16:24:53.832794', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/coconutoil.jpg', 12, 4.6, 'Coconut'),
+    (7, 180, '2024-06-01 16:27:08.727139', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/arnicamil.jpeg', 15, 4.3, 'Arnicamil'),
+    (8, 185, '2024-06-01 16:29:06.167187', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/items_image/massagetowel.png', 44.95, 4.2, 'MEGA');
+
+
+INSERT INTO post (created_at, member_id, post_id, content, image_url, title, category, url)
+VALUES
+    ('2024-06-01 13:17:20.319819', 1, 6, '림프절 마사지로 노폐물 배출! 『귀 마사지법』 [굿모닝 정보세상] 172회 20200313', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/earsmassage1.jpeg', '림프절 마사지로 노폐물 배출', 'EARS', 'https://www.youtube.com/watch?v=Fz9d04yC0qQ'),
+    ('2024-06-01 13:18:30.965646', 1, 7, '하수구가 막히면 답이 없듯이, 우리 몸속의 하수구와 같은 림프절도 림프액이 잘 통과하려면 잘 뚫어줘야 한다.', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/earsmassage2.jpeg', '귀 림프 순환을 돕는 ✌마사지!', 'EARS', 'https://program.imbc.com/Enews/Detail/goodday?idx=301628'),
+    ('2024-06-01 13:07:28.006705', 1, 3, '50만명이 효과를 본 광대 작아지는 법 / 앞광대, 옆광대, 45도광대 주목! How to make your cheekbones smaller.', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/cheekmassage1.jpeg', '50만명이 효과를 본 광대 작아지는 법', 'FACE', 'https://www.youtube.com/watch?v=h47n1kPnkyk'),
+    ('2024-06-01 13:08:38.128005', 1, 4, 'ENG) 옆광대 앞광대 모든광대 작아지는 법❗️ 끝판왕 마사지❗️ 따라만하세요..👍 (F. 영상 끝에 풀영상 첨부) How to Reduce Cheekbones', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/cheekmassage2.jpeg', '옆광대 앞광대 모든광대 작아지는 법', 'FACE', 'https://www.youtube.com/watch?v=FdsFbz9K5tc'),
+    ('2024-06-01 13:09:27.164015', 1, 5, 'SUB) 집에서 얼굴 작아지는 셀프 경락 (광대 없애는법)💆🏻‍♀️⎮ 미소정 MisoJeong', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/cheekmassage3.jpeg', '얼굴 작아지는 셀프 경락', 'FACE', 'https://www.youtube.com/watch?v=2px511s3SOw'),
+    ('2024-06-01 13:30:15.783149', 1, 8, '[얼굴붓기/사각턱/이중턱] 수술 없이 얼굴이 작아지는 셀프 경락 마사지법', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/chinmassage1.jpeg', '셀프 경락 마사지법', 'CHIN', 'https://www.youtube.com/watch?v=sAxq-XzFTaA'),
+    ('2024-06-01 13:31:01.713361', 1, 9, '얼굴 작아지는 팁! 턱 근육 빼는법 | 숙면 스트레칭 #5', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/chinmassage2.jpeg', '턱 근육 빼는법', 'CHIN', 'https://www.youtube.com/watch?v=v8FhMrb1yAs'),
+    ('2024-06-01 10:17:45.878431', 1, 1, '눈 피로 bye~ 눈 건강에 도움 주는 ▸마사지법◂ TV CHOSUN 210818 방송 | [굿모닝 정보세상] 469회 | TV조선', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/eyemassage1.jpeg', '눈 피로 bye~ 눈 건강에 도움 주는', 'EYES', 'https://www.youtube.com/watch?v=Vtov8dbKEhE'),
+    ('2024-06-01 12:06:58.243513', 1, 2, '힐링건강TIP [힐링리포트] 피로한 내 눈을 위한 건강 눈 마사지', 'https://rmwiqrerffapmjylrcir.supabase.co/storage/v1/object/public/massage_image/eyemassage2.jpeg', '피로한 내 눈을 위한 건강 눈 마사지', 'EYES', 'https://healingfactory.co.kr/board/board_view.php?bc_code=tip&bd_idx=1878');
+
+INSERT INTO user (activated, created_at, user_id, nickname, username, password)
+VALUES
+    (false, '2024-06-01 10:17:19.914148', 1, NULL, 'sunwupark', '1234'),
+    (false, '2024-06-01 10:17:24.144477', 2, NULL, 'hanseungheon', '1234'),
+    (false, '2024-06-01 10:17:28.119014', 3, NULL, 'songyongwoo', '1234'),
+    (false, '2024-06-01 10:17:34.616202', 4, NULL, 'singsangyong', '1234'),
+    (false, '2024-06-01 10:17:42.542078', 5, NULL, 'minbyungsoo', '1234');
+
